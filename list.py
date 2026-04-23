2d17e9f437c34bfe4d5d65cb11adce3fe9df24dc
mani maran <149583643+maranmanimqni@users.noreply.github.com>
2026-04-23 19:35:27 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,36 @@
+
+# list=[1,2,3,4,5]
+# for i in list:
+#     print(i)
+
+
+# list=[1,2,3,4,5,6]
+# for i in list:
+#     if i%2==0:
+#         print(i)
+
+# list=[1,2,3,4,5,6]
+# for i in list:
+#     if i %2 != 0:
+#         print(i)
+
+
+# list=[1,2,3,4,5]
+# total=0
+# for i in list:
+#    total +=1
+# print(total)
+
+# list=[10,50,5,40,15]
+# largest=list[0]
+# for i in list:
+#     if i>largest:
+#         largest=i
+#         print(largest)
+
+# list=[10,50,5,40,15]
+# smaller=list[0]
+# for i in list:
+#     if i<smaller:
+#         smaller=i
+#         print(smaller)
